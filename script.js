ab937fe944a1bfc926d79fd66850056e7967f2cf
Ed Smilovici <edsmilovici@gmail.com>
2017-11-07 11:55:54 -0500

    Refs #3 Create playlist consisting of most recently like music spotifyWebApiWrapper.js: - Removed wrappers success parameters and instead returns the request to be treated as a promise - Changed createPlaylist method to automatically pick up userId when available and handle it when not - Created addTracksToPlaylist and getLogedInUserInfo methods; the later simply gets available user info and stores when the constructor is called with a accessToken script.js: - Created createRecentlyLikedPlaylist and createNewPlaylistAndAddMusic functions

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+
+var spotify = null;
+
+function createRecentlyLikedPlaylist (){
+    if (spotify !== null) {
+        var requestLikedMusic = spotify.getLikedMusic();
+        requestLikedMusic.done(function (data) {
+            var spotifyTrackUris = '';
+            data.items.forEach(function(item) {
+                if (spotifyTrackUris !== ''){
+                    spotifyTrackUris += ',';
+                }
+                spotifyTrackUris += item.track.uri;
+            }, this);
+            createNewPlaylistAndAddMusic('Most recently liked songs as of ' + new Date(), spotifyTrackUris);
+        });
+    } else {
+        console.log('Please login to spotify to use this feature');
+    }
+}
+
+function createNewPlaylistAndAddMusic (playlistName, spotifyTrackUris){
+    var createNewPlaylist = spotify.createPlaylist(playlistName);
+    createNewPlaylist.done(function (data) {
+        spotify.addTracksToPlaylist(data.id, spotifyTrackUris);
+    })
+}
+
 $(document).ready(function () {
-    var spotify = new spotifyWebApi();
+    spotify = new spotifyWebApi();
     $('#loginButton').click(spotify.login);
 });
